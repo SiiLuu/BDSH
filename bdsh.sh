7eedfbda5aa9678883e4f97882105b29330d444b
Simao <lucas.simao@epitech.eu>
2020-01-13 13:44:42 +0100

    MODIF bdsh.sh

--- bdsh.sh
+++ bdsh.sh
@@ -1,14 +1,33 @@
 #!/bin/sh
 
-i=1
-fullName=0
-
 check_args () {
-   
+    if [ "$1" = "-h" -a -z "$2" ];then
+        echo "Usage: ./bdsh [OPTION]... [COMMAND] [REQUEST]"
+        echo "OPTION:"
+        echo "  -h				  display usage"
+        echo "  -f FILE			json database file"
+        echo "  -j                json formated output for select command"
+        exit 0
+    elif [ -z "$4" -o "$1" != "-f" ];then
+        echo "Usage: ./bdsh [OPTION]... [COMMAND] [REQUEST]"
+        echo "OPTION:"
+        echo "  -h				  display usage"
+        echo "  -f FILE			json database file"
+        echo "  -j                json formated output for select command"
+        exit 1
+    fi
 }
 
 main () {
-    
+    check_args "$@"
 }
 
 main "$@"
+
+####ERRORS
+##ARGV < 5
+##PAS DE -f
+##PAS FICHIER JSON APRES -f
+##SI ARGV 3 != create/insert/select/update/delete/describe
+##REGARDER QUE DESC_$TABLE ET DATA_$TABLE EXISTENT
+##PAS DE '=' ENTRE LE NOM ET LA VALEUR DE CHAQUE VARIABLE
